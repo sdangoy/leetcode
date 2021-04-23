417a0171cb152202f64e1ab0d30a9969bb9e4169
Stefan Dangoy <stefan.dangoy@gmail.com>
2021-04-22 22:30:37 -0700

    Added first solution to #20

---
+++ #20 Valid Parentheses/README.md
@@ -0,0 +1,44 @@
+# (EASY) 20. Valid Parentheses
+Given a string <code>s</code> containing just the characters <code>'('</code>, <code>')'</code>, <code>'{'</code>, <code>'}'</code>, <code>'['</code>, <code>']'</code>, determine if the input string is valid.
+
+Any input string is valid if:
+<ol>
+    <li>Open brackets must be closed by the same type of brackets.</li>
+    <li>Open brackets must be closed in the correct order.</li>
+</ol>
+
+**Example 1:** <br>
+    <pre>
+        <b>Input:</b> s = "()"
+        <b>Output:</b> true
+    </pre>
+
+**Example 2:** <br>
+    <pre>
+        <b>Input:</b> s = "()[]{}"
+        <b>Output:</b> true
+    </pre>
+
+**Example 3:** <br>
+    <pre>
+        <b>Input:</b> s = "(]"
+        <b>Output:</b> false
+    </pre>
+
+**Example 4:** <br>
+    <pre>
+        <b>Input:</b> s = "([)]"
+        <b>Output:</b> false
+    </pre>
+
+**Example 5:** <br>
+    <pre>
+        <b>Input:</b> s = "{[]}"
+        <b>Output:</b> true
+    </pre>
+
+**Constraints:**
+<ul>
+    <li><code>1 <= s.length <= 10<super>4</super></code></li>
+    <li><code>s</code> consists of parentheses only <code>'()[]{}'</code>.</li>
+</ul>
---
+++ #20 Valid Parentheses/solution1.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    let currentOpenBracket = '';
+    let bracketStack = []; 
+    
+    if (s.length <= 1 || s[0] === ')' || s[0] === ']' || s[0] === '}') { // Edge cases to keep in mind that should return false
+        return false;
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            bracketStack.push(s[i]);
+            continue;
+        }
+        
+        currentOpenBracket = bracketStack.pop()
+        if (s[i] === ')' && currentOpenBracket != '(') { // For each ending bracket, check if it matches the latest opening bracket. Lots of repeated code here! Create a new function?
+            return false;
+        }
+        
+        if (s[i] === ']' && currentOpenBracket != '[') {
+            return false;
+        }
+        
+        if (s[i] === '}' && currentOpenBracket != '{') {
+            return false;
+        }
+    }
+    
+    if (bracketStack.length > 0) { // If there are any opening brackets remaining in Stack, then there aren't enough matching closing brackets!
+        return false;
+    }
+    
+    return true;
+};
+/* RESULTS: 
+Runtime: 120ms, faster than 5.11% of JavaScript submissions
+Memory Usage: 40.1 MB, less than 35.56% of JavaScript submissions
+O(n) runtime where n is the length of the string input because it iterates through the string only once!
+O(n) space where n is the length of the string input.
+*/
